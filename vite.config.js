e1acd0b4e1747608d5a91d3cb8b4cce85674d935
LonitLu <dimasik17021989@gmail.com>
2024-04-16 21:21:50 +0500

    upd

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://lonitlu.github.io/study-practice-react/",
   plugins: [react()],
-})
+});
