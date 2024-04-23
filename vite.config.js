59f150662bf2a0458bea102e3823d787260e08df
LonitLu <dimasik17021989@gmail.com>
2024-04-23 23:25:36 +0500

    upd

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://lonitlu.github.io/",
+  base: "https://lonitlu.github.io/study-practice-react/",
   plugins: [react()],
 });
