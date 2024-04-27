a7f10d18caecebbfbe4a9ce3d024b741d14336b6
LonitLu <dimasik17021989@gmail.com>
2024-04-27 12:17:45 +0500

    HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import state from "./redux/state.js";
 import { addPost } from "./redux/state.js";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 addPost("test test");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <App state={state} />
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
