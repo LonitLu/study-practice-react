5e35f2eb32eaf008f967f7a80821601fde7e5f87
LonitLu <dimasik17021989@gmail.com>
2024-05-26 16:22:08 +0500

    подключил аксиос, получаю данный юзеров по апи

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,36 +1,43 @@
+import axios from "axios";
 import styles from "./Users.module.css";
+import userPhoto from "../../assets/images/user.jpg"
 
 let Users = (props) => {
   if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        photoUrl:
-          "https://avatars.mds.yandex.net/i?id=65143c59e9df102a346b674e873d25e01b5514d1-12753080-images-thumbs&n=13",
-        followed: false,
-        fullName: "Dmitri",
-        status: "I am BOSS",
-        location: { city: "Tyumen", country: "Russia" },
-      },
-      {
-        id: 2,
-        photoUrl:
-          "https://avatars.mds.yandex.net/i?id=65143c59e9df102a346b674e873d25e01b5514d1-12753080-images-thumbs&n=13",
-        followed: true,
-        fullName: "Dima",
-        status: "No, I am BOSS",
-        location: { city: "Omsk", country: "Russia" },
-      },
-      {
-        id: 3,
-        photoUrl:
-          "https://avatars.mds.yandex.net/i?id=65143c59e9df102a346b674e873d25e01b5514d1-12753080-images-thumbs&n=13",
-        followed: false,
-        fullName: "Lonit",
-        status: "I am big BOSS",
-        location: { city: "Moscow", country: "Russia" },
-      },
-    ]);
+    axios.get("https://api.slingacademy.com/v1/sample-data/users").then(response => {
+    props.setUsers(response.data.users);
+    });
+
+
+    // props.setUsers([
+    //   {
+    //     id: 1,
+    //     photoUrl:
+    //       "https://avatars.mds.yandex.net/i?id=65143c59e9df102a346b674e873d25e01b5514d1-12753080-images-thumbs&n=13",
+    //     followed: false,
+    //     fullName: "Dmitri",
+    //     status: "I am BOSS",
+    //     location: { city: "Tyumen", country: "Russia" },
+    //   },
+    //   {
+    //     id: 2,
+    //     photoUrl:
+    //       "https://avatars.mds.yandex.net/i?id=65143c59e9df102a346b674e873d25e01b5514d1-12753080-images-thumbs&n=13",
+    //     followed: true,
+    //     fullName: "Dima",
+    //     status: "No, I am BOSS",
+    //     location: { city: "Omsk", country: "Russia" },
+    //   },
+    //   {
+    //     id: 3,
+    //     photoUrl:
+    //       "https://avatars.mds.yandex.net/i?id=65143c59e9df102a346b674e873d25e01b5514d1-12753080-images-thumbs&n=13",
+    //     followed: false,
+    //     fullName: "Lonit",
+    //     status: "I am big BOSS",
+    //     location: { city: "Moscow", country: "Russia" },
+    //   },
+    // ]);
   }
   return (
     <div>
@@ -38,7 +45,7 @@ let Users = (props) => {
         <div key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} className={styles.user__photo} />
+              <img src={u.profile_picture != null ? u.profile_picture : userPhoto} className={styles.user__photo} />
             </div>
             <div>
               {u.followed ? (
@@ -62,12 +69,14 @@ let Users = (props) => {
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
-              <div>{u.status}</div>
+              <div>
+                {u.first_name} {u.last_name}
+              </div>
+              {/* <div>{u.last_name}</div> */}
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div>{u.country}</div>
+              <div>{u.city}</div>
             </span>
           </span>
         </div>
